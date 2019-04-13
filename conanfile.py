c3dde1e640e7984a21b19ab992e4964b54285995
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2019-04-13 09:29:52 +0200

    added dsound to libs

--- conanfile.py
+++ conanfile.py
@@ -68,7 +68,8 @@ class RtAudioConan(ConanFile):
             if not self.options.shared:
                 release_libs = ["{}_static".format(self._rtaudio_libname)]
                 debug_libs = ["{}_staticd".format(self._rtaudio_libname)]
-                self.cpp_info.libs = ["winmm"]
+                self.cpp_info.libs = ["dsound"]
+
             else:
                 debug_libs = ["{}d".format(self._rtaudio_libname)]
 
